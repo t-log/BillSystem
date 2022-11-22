7956b66fd8e4fb737b9614a4cd92c76958e269cb
akhil <akhil.paul@ymail.com>
2022-11-22 12:11:46 +0530

    views menu

--- BillSystem.java
+++ BillSystem.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 class Customer{
+
+    static int teaPrice =10;
+    static int coffeePrice = 15;
+    static int snackPrice = 10;
+    int totalPrice = 0;
+
     Customer(){
         System.out.println("***ORDER MENU***\n" +
                 "Enter Option:\n" +
@@ -9,7 +16,6 @@ class Customer{
                 "3.Snacks \n" +
                 "0.Exit");
         Scanner sd = new Scanner(System.in);
-        int totalPrice = 0;
         boolean flagTwo = true;
         while (flagTwo){
             System.out.println("Select option:");
@@ -18,17 +24,17 @@ class Customer{
                 case 1:
                     System.out.println("Enter quantity for Tea:");
                     int teaQuantity = sd.nextInt();
-                    totalPrice += 10*teaQuantity;
+                    totalPrice += teaPrice*teaQuantity;
                     break;
                 case 2:
                     System.out.println("Enter quantity for Coffee:");
                     int coffeeQuantity = sd.nextInt();
-                    totalPrice += 15*coffeeQuantity;
+                    totalPrice += coffeePrice*coffeeQuantity;
                     break;
                 case 3:
                     System.out.println("Enter quantity for Snacks:");
                     int snacksQuantity = sd.nextInt();
-                    totalPrice += 25*snacksQuantity;
+                    totalPrice += snackPrice*snacksQuantity;
                     break;
                 case 0:
                     System.out.println("Total price ="+totalPrice);
@@ -41,8 +47,11 @@ class Customer{
 }
 public class BillSystem {
 
+
+
     public static void main(String[] args) {
         boolean flag = true;
+        ArrayList<Customer> customArray = new ArrayList<Customer>();
         while (flag) {
             System.out.println("***MENU***\n" +
                     "Enter Option:\n" +
@@ -53,13 +62,17 @@ public class BillSystem {
                     "0.Exit");
             Scanner sc = new Scanner(System.in);
             int option = sc.nextInt();
+
             switch (option) {
                 case 1:
                     Customer c = new Customer();
-
-
+                    customArray.add(c);
                 case 2:
+
                 case 3:
+                    for (Customer obj:customArray) {
+                        System.out.println(obj.totalPrice);
+                    }
                 case 4:
                 case 0:
             }
